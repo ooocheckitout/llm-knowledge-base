78610ec5c84438ca7d31b2df82fe0c8429b2a5a3
hearthfire12 <solomoychenkoo@gmail.com>
2025-04-01 20:23:58 +0200

    use fake llm with history

--- infobase/lileg_agent.py
+++ infobase/lileg_agent.py
@@ -1,6 +1,6 @@
 import logging
 import os
-from typing import Optional, Annotated
+from typing import Optional
 from typing import TypedDict
 
 from dotenv import load_dotenv
@@ -9,15 +9,15 @@ from langchain.globals import set_llm_cache
 from langchain.storage import LocalFileStore
 from langchain_chroma import Chroma
 from langchain_community.cache import SQLiteCache
+from langchain_community.llms.fake import FakeListLLM
 from langchain_core.chat_history import BaseChatMessageHistory, InMemoryChatMessageHistory
-from langchain_core.messages import AnyMessage
+from langchain_core.messages import BaseMessage
 from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.runnables import RunnableConfig
-from langchain_core.runnables import RunnableWithMessageHistory
+from langchain_core.runnables import RunnableConfig, RunnableWithMessageHistory, RunnableLambda
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_openai import ChatOpenAI
-from langgraph.constants import START, END
-from langgraph.graph import StateGraph, add_messages
+from langgraph.constants import START
+from langgraph.graph import StateGraph, MessagesState
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -29,11 +29,14 @@ class GlobalState(TypedDict):
     sessions: dict[str, InMemoryChatMessageHistory]
 
 
-class PromptState(TypedDict):
-    messages: Annotated[list[AnyMessage], add_messages]
-    context: Annotated[list[AnyMessage], add_messages]
+class VariablesDict(TypedDict):
     question: str
-    answer: str
+    context: list[str]
+
+
+class PromptState(MessagesState):
+    template: str
+    variables: VariablesDict
 
 
 class ChatOpenRouter(ChatOpenAI):
@@ -46,46 +49,32 @@ class ChatOpenRouter(ChatOpenAI):
         super().__init__(base_url=base_url, api_key=api_key, model=model, **kwargs)
 
 
-# llm = ChatOpenRouter(
-#     model="deepseek/deepseek-chat-v3-0324:free",
-#     temperature=0.7,
-#     max_completion_tokens=1024,
-# )
-
-from langchain.chains import LLMChain
-from langchain_huggingface import HuggingFaceEndpoint
-
-llm = HuggingFaceEndpoint(
-    model="openai-community/gpt2",
-    max_new_tokens=512,
-    temperature=0.7,
+llm = ChatOpenRouter(
+    model="deepseek/deepseek-chat-v3-0324:free",
+    temperature=0.3,
+    max_completion_tokens=1024,
 )
+llm = FakeListLLM(responses=["YO!"])
 
-prompt = ChatPromptTemplate.from_messages([
-    ("system", "You are an assistant for question-answering tasks."),
-    ("system", "Use the following pieces of retrieved context to answer the question."),
-    ("system", "If you don't know the answer, just say that you don't know."),
-    ("system", "Use five sentences maximum and keep the answer concise."),
-    ("placeholder", "{context}"),
-    ("user", "{question}"),
-])
+# llm = HuggingFaceEndpoint(
+#     model="openai-community/gpt2",
+#     temperature=0.7,
+# )
 
 global_state = GlobalState(sessions={})
 
 
-def get_by_session_id(session_id: str) -> BaseChatMessageHistory:
+def get_message_history_by_session_id(session_id: str) -> BaseChatMessageHistory:
     if session_id not in global_state["sessions"]:
         global_state["sessions"][session_id] = InMemoryChatMessageHistory()
 
     return global_state["sessions"][session_id]
 
 
-chain = RunnableWithMessageHistory(
-    prompt | llm,
-    get_by_session_id,
-    input_messages_key="question",
-    history_messages_key="history",
-)
+# embeddings = HuggingFaceEndpointEmbeddings(
+#     model="sentence-transformers/all-MiniLM-L6-v2",
+#     task="feature-extraction",
+# )
 
 embeddings = HuggingFaceEmbeddings(
     model_name="sentence-transformers/all-MiniLM-L6-v2",
@@ -103,40 +92,87 @@ vector_store = Chroma(
 )
 
 
-def retrieve_context(state: PromptState):
+def retrieve_similarities(state: PromptState, config: RunnableConfig):
     documents = vector_store.similarity_search(
-        query=state["question"],
+        query=state["variables"]["question"],
         k=12,
+        filter={"session_id": config["configurable"]["session_id"]},
     )
-    return {"context": [x.page_content for x in documents]}
+    return {"context": [x.page_content for x in documents] + state["variables"]["context"]}
 
 
-def chatbot(state: PromptState, config: RunnableConfig):
-    messages = chain.invoke({"question": state["question"], "context": state["context"]}, config)
+def trim_history(state: dict, max_length: int = 1):
+    if len(state["history"]) > max_length:
+        state["history"] = state["history"][:max_length]
+        return state
+
+    return state
+
+
+def list_variable_parser(state: dict):
+    for var_key, var_value in state.items():
+        if not isinstance(var_value, list):
+            continue
+
+        if all(isinstance(x, BaseMessage) for x in var_value):
+            var_value = [x.content for x in var_value]
+
+        state[var_key] = "\n\n".join([x for x in var_value])
+
+    return state
+
+
+def chatbot_with_history(state: PromptState, config: RunnableConfig):
+    prompt = ChatPromptTemplate.from_template(state["template"])
+    chain_with_history = RunnableWithMessageHistory(
+        RunnableLambda(trim_history) | RunnableLambda(list_variable_parser) | prompt | llm,
+        get_message_history_by_session_id,
+        input_messages_key="question",
+        history_messages_key="history",
+    )
+    messages = chain_with_history.invoke({**state["variables"]}, config)
     return {"messages": messages}
 
 
 builder = StateGraph(state_schema=PromptState)
-builder.add_node(retrieve_context)
-builder.add_node(chatbot)
-builder.add_edge(START, "retrieve_context")
-builder.add_edge("retrieve_context", "chatbot")
-builder.add_edge("chatbot", END)
+builder.add_sequence([retrieve_similarities, chatbot_with_history])
+builder.add_edge(START, "retrieve_similarities")
 graph = builder.compile()
-
-set_llm_cache(SQLiteCache(database_path=".cache/completions"))
 
 if __name__ == "__main__":
     vector_store = vector_store.from_texts(["Capital of Poland is Niagara."], cached_embedder)
 
+    set_llm_cache(SQLiteCache(database_path=".cache/completions"))
+
+    template = """
+You are a helpful assistant. Answer the question based on the context.
+Context: {context}
+Combine the chat history and follow up question into a standalone question:
+History: {history}
+Question: {question}
+Answer the standalone question based on the context.
+"""
     graph.invoke(
-        {"question": "Hello! My name is Oleh."},
-        {"configurable": {"session_id": "1"}}
+        PromptState(
+            template=template,
+            variables=VariablesDict(
+                question="What is the capital of Poland?",
+                context=["Capital of Poland is Mumbai."]
+            ),
+            messages=[]
+        ),
+        RunnableConfig(configurable={"session_id": "1"}),
     )
     print(global_state["sessions"]["1"])
 
     graph.invoke(
-        {"question": "Yo! What is the capital of Poland?"},
-        {"configurable": {"session_id": "1"}}
+        PromptState(
+            template=template,
+            variables=VariablesDict(question="What is the capital of Poland?", context=[]),
+            messages=[]
+        ),
+        RunnableConfig(configurable={"session_id": "1"}),
     )
     print(global_state["sessions"]["1"])
+
+set_llm_cache(SQLiteCache(database_path=".cache/completions"))
