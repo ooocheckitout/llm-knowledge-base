0c14d9741ab4b8affd74a9218b55cc26d66c1268
Oleh Solomoichenko <solomoychenkoo@gmail.com>
2026-03-27 09:32:06 +0100

    remove obsolete

--- src/search_bot/handlers/error.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-from telegram.ext import ContextTypes
-
-logger = logging.getLogger(__name__)
-
-
-async def error_handler(update: object, context: ContextTypes.DEFAULT_TYPE) -> None:
-    logger.warning(f'Update "{update}" caused error "{context.error}"')
